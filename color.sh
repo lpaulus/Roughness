d51d80fcf8d7c4ea63c6a6380fb02ea9c1d7c385
Léa Paulus <leapaulus28@gmail.com>
2019-08-15 13:32:37 +0200

    Call color.sh from process.sh

--- color.sh
+++ color.sh
@@ -7,32 +7,33 @@ smooth=$2
 curvature=$3
 average=$4
 dir="results/$name/${smooth}_${curvature}_${average}"
+root=$(pwd)
 if [ ! -d $dir ]; then
     echo "Directory $dir does not exists. Run 'bash process.sh' with the same argument to compute the roughness"
     exit 1
 fi
 cd $dir
-../../../build/Color normalised.off original_kmax.txt
+$root/build/Color normalised.off original_kmax.txt
 mv output.off original_kmax.off
-../../../build/Color normalised.off original_kmax.txt -s
+$root/build/Color normalised.off original_kmax.txt -s
 mv output.off original_kmax_sqrt.off
-../../../build/Color smooth.off smooth_kmax.txt
+$root/build/Color smooth.off smooth_kmax.txt
 mv output.off smooth_kmax.off
-../../../build/Color smooth.off smooth_kmax.txt -s
+$root/build/Color smooth.off smooth_kmax.txt -s
 mv output.off smooth_kmax_sqrt.off
-../../../build/Color normalised.off original_kav.txt
+$root/build/Color normalised.off original_kav.txt
 mv output.off original_kav.off
-../../../build/Color normalised.off original_kav.txt -s
+$root/build/Color normalised.off original_kav.txt -s
 mv output.off original_kav_sqrt.off
-../../../build/Color smooth.off smooth_kav.txt
+$root/build/Color smooth.off smooth_kav.txt
 mv output.off smooth_kav.off
-../../../build/Color smooth.off smooth_kav.txt -s
+$root/build/Color smooth.off smooth_kav.txt -s
 mv output.off smooth_kav_sqrt.off
-../../../build/Color normalised.off roughness.txt
+$root/build/Color normalised.off roughness.txt
 mv output.off normalised_roughness.off
-../../../build/Color normalised.off roughness.txt -s
+$root/build/Color normalised.off roughness.txt -s
 mv output.off normalised_roughness_sqrt.off
-../../../build/Color smooth.off roughness.txt
+$root/build/Color smooth.off roughness.txt
 mv output.off smooth_roughness.off
-../../../build/Color smooth.off roughness.txt -s
+$root/build/Color smooth.off roughness.txt -s
 mv output.off smooth_roughness_sqrt.off
--- process.sh
+++ process.sh
@@ -7,6 +7,7 @@ smooth=$2
 curvature=$3
 average=$4
 dir="results/$name/${smooth}_${curvature}_${average}"
+root=$(pwd)
 if [ ! -f data/$name.obj ]; then
     echo "No $name.obj in data directory"
     exit 1
@@ -17,22 +18,12 @@ if [ -d $dir ]; then
 fi
 mkdir -p $dir
 cd $dir
-../../../build/Roughness ../../../data/$name.obj $smooth $curvature $average
+$root/build/Roughness $root/data/$name.obj $smooth $curvature $average
 if [ $? -ne 0 ]; then
     echo "Roughness failed"
-    cd ../../../
+    cd $root
     rm -r $dir
     exit $?
 fi
-../../../build/Color normalised.off original_kmax.txt
-mv output.off original_kmax.off
-../../../build/Color smooth.off smooth_kmax.txt
-mv output.off smooth_kmax.off
-../../../build/Color normalised.off original_kav.txt
-mv output.off original_kav.off
-../../../build/Color smooth.off smooth_kav.txt
-mv output.off smooth_kav.off
-../../../build/Color normalised.off roughness.txt
-mv output.off normalised_roughness.off
-../../../build/Color smooth.off roughness.txt
-mv output.off smooth_roughness.off
+cd $root
+bash color.sh $name $smooth $curvature $average
